ebd6797ed95773a835595af1500e2390b307c734
unknown <afif.maliki21@gmail.com>
2022-12-21 23:51:16 +0700

    adding migration data

--- database/migrations/2022_12_21_163950_create_tables_table.php
+++ database/migrations/2022_12_21_163950_create_tables_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tables', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
+            $table->integer('guest_number');
+            $table->string('status')->default('avaliable');
+            $table->string('location');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tables');
     }
 };
---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+// var myNav = document.getElementById("mynav");
+// window.onscroll = function (e) {
+//     if (window.pageYOffset >= 100) {
+//         myNav.classList.add(
+//             "bg-white",
+//             "ease-in-out",
+//             "duration-300",
+//             "text-slate-900"
+//         );
+//         // myNav.classList.remove("");
+//     } else {
+//         // myNav.classList.add("bg-transparent");
+//         myNav.classList.remove("bg-white");
+//         myNav.classList.remove("text-slate-900");
+//         myNav.classList.add("text-white");
+//     }
+// };
+
+const { default: Typed } = require("typed.js");
+
+// const cards = document.querySelectorAll(".card");
+
+// cards.forEach((card) => {
+//     ScrollReveal().reveal(card, { delay: 500 });
+// });
+const span = document.querySelector("#typed");
