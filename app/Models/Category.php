8c85949e490b10ecd97735e17a036b9a1b17695b
unknown <afif.maliki21@gmail.com>
2022-12-25 03:51:46 +0700

    adding eloquent relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,6 +12,11 @@ class Category extends Model
 
     protected $fillable = ["name", "description", "image"];
 
+    public function menu()
+    {
+        return $this->hasMany(Menu::class);
+    }
+
     public function getRouteKeyName()
     {
         return 'slug';
