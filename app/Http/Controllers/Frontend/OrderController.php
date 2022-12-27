f8e14377477064e80076cc39eed65779f263955a
unknown <afif.maliki21@gmail.com>
2022-12-28 00:06:32 +0700

    make menu page and controller

--- app/Http/Controllers/Frontend/OrderController.php
+++ app/Http/Controllers/Frontend/OrderController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Menu;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class OrderController extends Controller
 {
     public function index()
     {
-        return view('order.index');
+        $menus = Menu::all();
+        return view('order.index', compact('menus'));
     }
 }
