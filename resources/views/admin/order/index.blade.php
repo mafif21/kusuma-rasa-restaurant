4d4e537f99e8e6b6ccfc5fcbfda2a0f55fa21df1
unknown <afif.maliki21@gmail.com>
2023-01-06 03:54:35 +0700

    removing useless component at admin page

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -2,23 +2,11 @@
   <x-slot name="title">Admin Category</x-slot>
 
   <x-slot name="slot">
-    <div class="flex justify-between mb-5 items-center">
-        <div class="relative">
-            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8  4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-            </div>
-                <input type="text" id="table-search" class="block p-3 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-orange-200 focus:border-orange-200 border-gray-100 placeholder-gray-400 " placeholder="Search for items">
-        </div>
-        <div>
-            <a href="{{ route('admin.category.create') }}" class="text-white bg-orange-400 font-medium rounded-lg text-sm px-5 py-2.5">Add Category</a>
-        </div>
-    </div>
-
     <div class="overflow-x-auto shadow-md relative sm:rounded-lg mx-auto">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
             <caption class="p-5 text-lg font-semibold text-left text-gray-900 ">
-                Order
-                <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Halo admin {{ auth()->user()->name}} silahkan tambah, edit atau hapus menu yang tersedia, Have a nice day</p>
+                Order Page
+                <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Halo admin {{ auth()->user()->name }} semoga harimu bahagia, silahkan tambah, edit, hapus menu yang sudah ada pada halaman Order ini</p>
             </caption>
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                     <tr>
