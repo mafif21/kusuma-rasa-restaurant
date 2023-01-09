693bc25e78f20922d4551cdedd2fb2686315d8a0
rikyindrasakti <115932473+rikyindrasakti@users.noreply.github.com>
2023-01-09 21:07:24 +0700

    update table

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -13,6 +13,10 @@ class Table extends Model
     use HasFactory, Sluggable;
     protected $fillable = ['name', 'guest_number', 'status', 'location'];
 
+    protected $casts = [
+        "status" => TableStatus::class,
+        "location" => TableLocation::class
+    ];
 
 
     public function reservations()
