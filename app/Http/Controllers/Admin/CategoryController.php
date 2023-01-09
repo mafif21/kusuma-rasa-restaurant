137ebe1c33ea164f6d26cf67f2ccbe41dddf0a5b
rikyindrasakti <115932473+rikyindrasakti@users.noreply.github.com>
2023-01-09 21:14:21 +0700

    change category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -19,7 +19,7 @@ class CategoryController extends Controller
     public function index()
     {
         $categories = Category::all();
-        return view('admin.category.index', compact('categories'));
+        
     }
 
     /**
