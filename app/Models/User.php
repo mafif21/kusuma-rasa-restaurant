03b7b298d9eb7ad42ad404dbf0e600221fb48732
rikyindrasakti <115932473+rikyindrasakti@users.noreply.github.com>
2023-01-09 20:44:57 +0700

    update user

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,5 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 
 }
