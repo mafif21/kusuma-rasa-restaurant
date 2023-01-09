5bcdfcfe7d64d26fa4976aef6f18a6155fd9a497
alvinrenaldy <alvinrenaldy362@gmail.com>
2023-01-09 16:17:59 +0700

    adding controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    
+    public function index()
+    {
+        $categories = Category::all();
+        return view('home', compact('categories'));
+    }
 }
