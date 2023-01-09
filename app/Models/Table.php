a5b313a00cd9670eb30bc2cb6d25b790ed270942
rikyindrasakti <115932473+rikyindrasakti@users.noreply.github.com>
2023-01-09 21:09:52 +0700

    change table

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -6,7 +6,7 @@ use App\Enums\TableLocation;
 use Cviebrock\EloquentSluggable\Sluggable;
 use App\Enums\TableStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+
 
 class Table extends Model
 {
