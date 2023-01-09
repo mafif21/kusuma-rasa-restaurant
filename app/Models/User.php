cbe68932936f0d0cc7a7c67b87ae6a25885e3930
FarhanWijonarko <farhan.wijonarko1@gmail.com>
2023-01-09 20:39:56 +0700

    delete

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,8 +42,5 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function orders()
-    {
-        return $this->hasMany(Order::class);
-    }
+
 }
