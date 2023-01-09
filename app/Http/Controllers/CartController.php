b10c2ed4aacb8164eb34385ec01563ffe25498a6
unknown <afif.maliki21@gmail.com>
2023-01-09 15:54:05 +0700

    remove wrong component

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -17,29 +17,6 @@ class CartController extends Controller
         return view('cart.index', compact('carts'));
     }
 
-    public function addCart(Request $request)
-    {
-        $data = Menu::where('slug', $request->slug)->get()->first();
-        Cart::add($data->slug, $data->name, 1, $data->price, 0);
-
-        return to_route('food.index');
-    }
-
-    public function store(Request $request)
-    {
-        $validatedData = $request->validate([
-            'name' => 'required|max:255',
-            'qty' => 'required|integer',
-            'price' => 'required',
-        ]);
-
-        $validatedData['user_id'] = auth()->user()->id;
-        $validatedData['status'] = false;
-
-
-        Order::create($validatedData);
-        return to_route('cart.index')->with('success', 'Order Successs');
-    }
 
     public function update(Request $request, $key)
     {
