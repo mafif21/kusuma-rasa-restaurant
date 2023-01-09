9c308f16e84051b27c341e5e416b6e1cc4f08cb5
rikyindrasakti <115932473+rikyindrasakti@users.noreply.github.com>
2023-01-09 16:20:45 +0700

    delete anchor

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,6 @@
           <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
               <caption class="p-5 text-lg font-semibold text-left text-gray-900 ">
                   Order Information
-                  <a href="#">Recap Order</a>
               </caption>
                   <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                       <tr>
