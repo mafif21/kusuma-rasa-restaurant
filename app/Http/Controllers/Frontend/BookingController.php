8144e59f9eb783002989c572e4523fc2a050e9cc
FarhanWijonarko <farhan.wijonarko1@gmail.com>
2023-01-09 21:40:44 +0700

    update bookingcontroller

--- app/Http/Controllers/Frontend/BookingController.php
+++ app/Http/Controllers/Frontend/BookingController.php
@@ -8,7 +8,7 @@ use App\Rules\DateRule;
 use App\Rules\TimeRule;
 use App\Enums\TableStatus;
 use App\Models\Reservation;
-
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class BookingController extends Controller
