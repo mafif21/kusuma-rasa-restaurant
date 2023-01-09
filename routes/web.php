6a9079c883c8cad217b4ec72d1669337cd5cf39c
adnannaulih <adnannauliharahap@gmail.com>
2023-01-09 19:35:09 +0700

    UpdateRoutes

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // Route::view('/about', 'about')->name('about');
 });
