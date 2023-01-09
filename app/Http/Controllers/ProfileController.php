05d31f16eca4d19185458a12109d97f7bc83659d
FarhanWijonarko <farhan.wijonarko1@gmail.com>
2023-01-09 21:00:35 +0700

    add profilecontroller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -30,8 +30,7 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request)
     {
-       
-
+        $request->user()->fill($request->validated());  
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
