130b35b7c6476e34c6d51c32965153e1b6d3b4ed
adnannaulih <adnannauliharahap@gmail.com>
2023-01-09 19:36:46 +0700

    updateController

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -26,17 +26,6 @@ class ReservationController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $tables = Table::where("status", TableStatus::Available)->get();
-        return view('admin.reservation.create', compact('tables'));
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
