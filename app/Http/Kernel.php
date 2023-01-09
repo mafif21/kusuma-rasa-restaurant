f2e7f93edbb9dc1778da116b88ff2329609960dc
unknown <afif.maliki21@gmail.com>
2023-01-09 16:23:14 +0700

    deleted

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin' => Admin::class,
     ];
 }
