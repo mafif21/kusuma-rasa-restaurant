b6f0be38622df38fb1a60cf42eaf24fb35e29492
alvinrenaldy <alvinrenaldy362@gmail.com>
2023-01-09 20:17:49 +0700

    add food controller

--- app/Http/Controllers/Frontend/FoodController.php
+++ app/Http/Controllers/Frontend/FoodController.php
@@ -8,5 +8,15 @@ use App\Http\Controllers\Controller;
 
 class FoodController extends Controller
 {
-    
+    public function index()
+    {
+        $menus = Menu::latest()->filter(request(['search', 'category', 'author']))->get();
+        return view('foods.index', compact('menus'));
+    }
+
+    public function show($slug)
+    {
+        $food = Menu::where('slug', $slug)->get()->first();
+        return view('foods.detail', compact('food'));
+    }
 }
