fabf72fb079b8b126ccb0667c201369caaf0bf4d
FarhanWijonarko <farhan.wijonarko1@gmail.com>
2023-01-09 21:23:09 +0700

    delete

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -38,7 +38,7 @@ class ProfileController extends Controller
 
         $request->user()->save();
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        
     }
 
     /**
