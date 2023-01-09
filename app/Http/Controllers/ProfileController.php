15f20d1f33a6aebeb2500f4d1d14d96061612a87
rikyindrasakti <115932473+rikyindrasakti@users.noreply.github.com>
2023-01-09 21:12:27 +0700

    change profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
+
 
 class ProfileController extends Controller
 {
